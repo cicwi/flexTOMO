41e992cdc5e87ce4cc3610b372b987ad288970df
Alexander Skorikov <alexander.skorikov@gmail.com>
2024-09-12 16:43:45 +0200

    Remove unused functionality in build files

--- Makefile
+++ Makefile
@@ -29,7 +29,7 @@ BROWSER := python -c "$$BROWSER_PYSCRIPT"
 help:
 	@python -c "$$PRINT_HELP_PYSCRIPT" < $(MAKEFILE_LIST)
 
-clean: clean-build clean-pyc clean-test ## remove all build, test, coverage and Python artifacts
+clean: clean-build clean-pyc
 
 clean-build: ## remove build artifacts
 	rm -fr build/
@@ -44,27 +44,6 @@ clean-pyc: ## remove Python file artifacts
 	find . -name '*~' -exec rm -f {} +
 	find . -name '__pycache__' -exec rm -fr {} +
 
-clean-test: ## remove test and coverage artifacts
-	rm -fr .tox/
-	rm -f .coverage
-	rm -fr htmlcov/
-	rm -fr .pytest_cache
-
-lint: ## check style with flake8
-	flake8 flextomo tests
-
-test: ## run tests quickly with the default Python
-	python setup.py test
-
-test-all: ## run tests on every Python version with tox
-	tox
-
-coverage: ## check code coverage quickly with the default Python
-	coverage run --source flextomo setup.py test
-	coverage report -m
-	coverage html
-	$(BROWSER) htmlcov/index.html
-
 docs/.nojekyll:
 	touch docs/.nojekyll
 
@@ -76,14 +55,10 @@ docs: docs/.nojekyll install_dev ## generate Sphinx HTML documentation, includin
 	make -C doc_sources html
 	$(BROWSER) docs/index.html
 
-servedocs: docs ## compile the docs watching for changes
-	watchmedo shell-command -p '*.rst' -c '$(MAKE) -C doc_sources html' -R -D .
-
 install: clean ## install the package to the active Python's site-packages
-	python setup.py install
+	pip install .
 
 install_dev:
-	# https://stackoverflow.com/a/28842733
 	pip install -e .[dev]
 
 conda_package:
--- setup.py
+++ setup.py
@@ -1,74 +1,29 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""The setup script."""
-
 from setuptools import setup, find_packages
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-requirements = [
-    "numpy",
-    "astra-toolbox>1.9.0",
-    "tqdm",
-    "scipy",
-    "flexdata"]
-
-setup_requirements = [ ]
-
-test_requirements = [ ]
-
-dev_requirements = [
-    'autopep8',
-    'rope',
-    'jedi',
-    'flake8',
-    'importmagic',
-    'autopep8',
-    'black',
-    'yapf',
-    'snakeviz',
-    # Documentation
-    'sphinx',
-    'sphinx_rtd_theme',
-    'recommonmark',
-    # Other
-    'bumpversion',
-    'watchdog',
-    'coverage',
-
-    ]
-
 setup(
     author="Alex Kostenko",
-    author_email='a.kostenko@cwi.nl',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
     description="ASTRA-based cone beam tomography reconstructions.",
-    install_requires=requirements,
+    install_requires= [
+        "numpy",
+        "astra-toolbox>1.9.0",
+        "tqdm",
+        "scipy",
+        "flexdata"
+    ],
     license="GNU General Public License v3",
     long_description=readme,
-    include_package_data=True,
-    keywords='flextomo',
     name='flextomo',
     packages=find_packages(include=['flextomo']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    extras_require={ 'dev': dev_requirements },
+    extras_require={
+        'dev': [
+            'sphinx',
+            'sphinx_rtd_theme',
+            'recommonmark',
+        ]
+    },
     url='https://github.com/cicwi/flextomo',
     version='0.1.0',
-    zip_safe=False,
 )
