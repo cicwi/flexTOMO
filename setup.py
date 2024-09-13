2729a74051d619a2f76116e841d2a7fb3a7764dd
Alexander Skorikov <alexander.skorikov@gmail.com>
2024-09-13 11:36:43 +0200

    Update setup.py to follow the style of other flexbox packages

--- setup.py
+++ setup.py
@@ -4,27 +4,27 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 setup(
-    author="Alex Kostenko",
-    description="ASTRA-based cone beam tomography reconstructions.",
-    install_requires= [
-        "numpy",
-        "astra-toolbox>1.9.0",
-        "tqdm",
-        "scipy",
-        "flexdata"
-    ],
-    license="GNU General Public License v3",
-    long_description=readme,
-    long_description_content_type="text/markdown",
     name='flextomo',
+    version='1.0.0',
+    description='ASTRA-based cone beam tomography reconstructions',
+    url='https://github.com/cicwi/flextomo',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Alex Kostenko',
+    license='GNU General Public License v3',
     packages=find_packages(include=['flextomo']),
+    install_requires= [
+        'numpy',
+        'astra-toolbox>1.9.0',
+        'tqdm',
+        'scipy',
+        'flexdata'
+    ],
     extras_require={
         'dev': [
             'sphinx',
             'sphinx_rtd_theme',
             'myst-parser',
         ]
-    },
-    url='https://github.com/cicwi/flextomo',
-    version='1.0.0',
+    }
 )
